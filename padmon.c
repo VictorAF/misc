31d408745f0301cfff7c8f5d1e51e7e8a0d319fa
João Victor Fernandes <ra158043@students.ic.unicamp.br>
2018-05-07 11:08:22 -0300

    Adding first code10

--- padmon.c
+++ padmon.c
@@ -5,13 +5,13 @@
 #include "/usr/src/minix/include/minix/ipc.h"
 #include "/usr/src/minix/servers/pm/mproc.h"
 //#include "/usr/src/minix/kernel/arch/i386/include/archtypes.h"
-#include "/usr/src/minix/kernel/const.h"
-#include "/usr/src/minix/kernel/type.h"
-#include "/usr/src/minix/kernel/proc.h"
-
-#include "/usr/src/minix/servers/vfs/fproc.h"
-#include "/usr/src/minix/servers/vfs/const.h"
-#include "/usr/src/minix/servers/mfs/const.h"
+// #include "/usr/src/minix/kernel/const.h"
+// #include "/usr/src/minix/kernel/type.h"
+// #include "/usr/src/minix/kernel/proc.h"
+//
+// #include "/usr/src/minix/servers/vfs/fproc.h"
+// #include "/usr/src/minix/servers/vfs/const.h"
+// #include "/usr/src/minix/servers/mfs/const.h"
 
 
 void do_printpids();
