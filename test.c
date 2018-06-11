74e3625b8ebffadc6cd645438e9f1e5249068df0
João Victor Fernandes <ra158043@students.ic.unicamp.br>
2018-06-11 14:24:17 -0300

    v

--- test.c
+++ test.c
@@ -5,22 +5,22 @@
 
 int main()
 {
-	int pid[6];
+	int pid[20];
 	int i;
 	char processid[100];
 
-    for(i=1;i<=5;i++)
+    for(i=1;i<=10;i++)
     {
     	pid[i]=fork();
         if(pid[i]==0)
     	{
     		sprintf(processid,"%2d",i);
-    		execlp("./longrun0","./longrun0",processid,"100000","1000",NULL);
+    		execlp("./longrun","./longrun",processid,"100000","1000",NULL);
 
         }
 
     }
-    for(i=1;i<=5;i++)
+    for(i=1;i<=10;i++)
     {
     	wait(NULL);
     }
