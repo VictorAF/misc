a0b854f49d72a2f194e58047ab9321a4e3f72f40
João Victor Fernandes <ra158043@students.ic.unicamp.br>
2018-06-14 16:29:20 -0300

    v

--- simple-test2.c
+++ simple-test2.c
@@ -14,11 +14,17 @@ int main() {
   child_pid = fork();
 
   if(child_pid == 0){
-    for(i = 0; i < 10000000000000; i++);
+    i = 0;
+    while(i < 100000){
+      i++;
+    }
     printf("Finishing pid: %d\n", getpid());
   }
   else{
-    for(i = 0; i < 1000000000000000; i++);
+    i = 0;
+    while(i < 10000000){
+      i++;
+    }
     printf("Finishing pid: %d, parent of pid: %d\n", ppid, child_pid);
 
   }
